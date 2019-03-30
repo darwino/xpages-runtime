acdcde20ad34e2711d0561a2f67d554aadc63d2c
Jesse Gallagher <jesse@secondfoundation.org>
2019-03-30 15:34:49 -0400

    Improve XPagesService and bring over another test page

--- xpages-ios/xpages-ios-shared/src/main/java/com/darwino/xsp/ios/app/services/XPagesService.java
+++ xpages-ios/xpages-ios-shared/src/main/java/com/darwino/xsp/ios/app/services/XPagesService.java
@@ -23,14 +23,28 @@ import com.ibm.xsp.webapp.DesignerFacesServlet;
 public class XPagesService extends HttpService {
 	private DesignerFacesServlet delegate;
 	private JakartaConfigureCoreListener listener = new JakartaConfigureCoreListener();
+	private boolean initialized;
 
 	@Override
 	public void service(HttpServiceContext serviceContext) {
+		init(serviceContext);
+		
 		HttpServletRequest req = new DarwinoServletRequestWrapper(((ServletServiceContext)serviceContext).getHttpRequest());
 		HttpServletResponse res = ((ServletServiceContext)serviceContext).getHttpResponse();
-		ServletContext context = new DarwinoServletContextWrapper(((ServletServiceContext)serviceContext).getServletContext());
 		
-		if(this.delegate == null) {
+		System.out.println("path info: " + req.getPathInfo());
+		try {
+			delegate.service(req, res);
+		} catch (ServletException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private synchronized void init(HttpServiceContext serviceContext) {
+		if(!this.initialized) {
+			ServletContext context = new DarwinoServletContextWrapper(((ServletServiceContext)serviceContext).getServletContext());
 			ServletConfig conf = new DarwinoServletConfigWrapper(context);
 			ServletContextEvent sce = new ServletContextEvent(context);
 			listener.contextInitialized(sce);
@@ -43,15 +57,8 @@ public class XPagesService extends HttpService {
 			} catch (ServletException e) {
 				throw new RuntimeException(e);
 			}
-		}
-
-		try {
-			delegate.service(req, res);
-		} catch (ServletException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+			
+			this.initialized = true;
 		}
 	}
-
 }
--- xpages-ios/xpages-ios-shared/src/main/java/xsp/Nondomino.java
+++ xpages-ios/xpages-ios-shared/src/main/java/xsp/Nondomino.java
@@ -14,7 +14,14 @@ import com.ibm.xsp.page.compiled.PageExpressionEvaluator;
 import java.lang.String;
 import com.ibm.xsp.component.UIViewRootEx2;
 import com.ibm.xsp.component.UIIncludeComposite;
+import com.ibm.xsp.component.UIPassThroughTag;
 import com.ibm.xsp.component.UIPassThroughText;
+import com.ibm.xsp.component.xp.XspDiv;
+import com.ibm.xsp.component.xp.XspOutputText;
+import java.lang.Object;
+import javax.faces.el.ValueBinding;
+import com.ibm.xsp.component.xp.XspCommandButton;
+import com.ibm.xsp.component.xp.XspEventHandler;
 
 @SuppressWarnings("all")
 public class Nondomino extends AbstractCompiledPageDispatcher{
@@ -31,15 +38,25 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
         
         private static final ComponentInfo[] s_infos = new ComponentInfo[]{
             ComponentInfo.EMPTY_MARKUP, // 0 text
-            new ComponentInfo(false, new int[]{0}), // 1 layout
-            new ComponentInfo(false, new int[]{1}), // 2 view
+            new ComponentInfo(true, new int[]{0}), // 1 p
+            ComponentInfo.EMPTY_NORMAL, // 2 text2
+            new ComponentInfo(true, new int[]{2}), // 3 p2
+            new ComponentInfo(false, new int[]{3}), // 4 "refresher"
+            ComponentInfo.EMPTY_NORMAL, // 5 eventHandler
+            new ComponentInfo(false, new int[]{5}), // 6 "refresh"
+            new ComponentInfo(false, new int[]{1, 4, 6}), // 7 layout
+            new ComponentInfo(false, new int[]{7}), // 8 view
         };
         
         public NondominoPage() {
-            super(2, s_infos );
+            super(8, s_infos );
         }
         
         public int getComponentForId(String id) throws NoSuchComponentException { 
+            if( "refresher".equals(id) )
+                return 4;
+            if( "refresh".equals(id) )
+                return 6;
             return -1;
         }
         
@@ -47,12 +64,24 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
                 UIComponent parent, PageExpressionEvaluator evaluator)
                 throws NoSuchComponentException { 
             switch (id) {
-            case 2:
+            case 8:
                 return createView(context, parent, evaluator);
-            case 1:
+            case 7:
                 return createLayout(context, parent, evaluator);
+            case 1:
+                return createP(context, parent, evaluator);
             case 0:
                 return createText(context, parent, evaluator);
+            case 4:
+                return createRefresher(context, parent, evaluator);
+            case 3:
+                return createP2(context, parent, evaluator);
+            case 2:
+                return createText2(context, parent, evaluator);
+            case 6:
+                return createRefresh(context, parent, evaluator);
+            case 5:
+                return createEventHandler(context, parent, evaluator);
             }
             throw new NoSuchComponentException(id);
         }
@@ -73,11 +102,60 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
             return result;
         }
 
+        private UIComponent createP(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("p");
+            return component;
+        }
+
         private UIComponent createText(FacesContext context, 
                 UIComponent parent, PageExpressionEvaluator evaluator) {
             UIPassThroughText textComp = new UIPassThroughText();
             textComp.setText("Page loaded!");
             return textComp;
+        }
+
+        private UIComponent createRefresher(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspDiv result = new XspDiv();
+            setId(result, "refresher");
+            return result;
+        }
+
+        private UIComponent createP2(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("p");
+            return component;
+        }
+
+        private UIComponent createText2(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspOutputText result = new XspOutputText();
+            String sourceId = "refresher/p[1]/xp:text[1]/@value";
+            String valueExpr = "Random string: #{randomString.randomString}";
+            ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId,Object.class);
+            result.setValueBinding("value", value);
+            return result;
+        }
+
+        private UIComponent createRefresh(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspCommandButton result = new XspCommandButton();
+            result.setValue("Refresh");
+            setId(result, "refresh");
+            return result;
+        }
+
+        private UIComponent createEventHandler(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspEventHandler result = new XspEventHandler();
+            result.setRefreshId("refresher");
+            result.setSubmit(true);
+            result.setEvent("onclick");
+            result.setRefreshMode("partial");
+            return result;
         }
 
     }
