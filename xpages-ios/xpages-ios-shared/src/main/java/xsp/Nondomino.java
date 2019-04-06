de07df74bffc5332f57352981369beec469b0ac4
Jesse Gallagher <jesse@secondfoundation.org>
2019-04-06 13:58:53 -0400

    Add timestamp test to Nondomino.xsp

--- xpages-ios/xpages-ios-shared/src/main/java/xsp/Nondomino.java
+++ xpages-ios/xpages-ios-shared/src/main/java/xsp/Nondomino.java
@@ -17,9 +17,8 @@ import com.ibm.xsp.component.FacesPageProvider;
 import com.ibm.xsp.component.UIViewRootEx2;
 import com.ibm.xsp.component.UIIncludeComposite;
 import com.ibm.xsp.component.UIPassThroughTag;
-import com.ibm.xsp.component.UIPassThroughText;
-import com.ibm.xsp.component.xp.XspDiv;
 import com.ibm.xsp.component.xp.XspOutputText;
+import com.ibm.xsp.component.xp.XspDiv;
 import java.lang.Object;
 import javax.faces.el.ValueBinding;
 import com.ibm.xsp.component.xp.XspCommandButton;
@@ -39,26 +38,30 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
     public static class NondominoPage extends AbstractCompiledPage {
         
         private static final ComponentInfo[] s_infos = new ComponentInfo[]{
-            ComponentInfo.EMPTY_MARKUP, // 0 text
+            ComponentInfo.EMPTY_NORMAL, // 0 text
             new ComponentInfo(true, new int[]{0}), // 1 p
             ComponentInfo.EMPTY_NORMAL, // 2 text2
             new ComponentInfo(true, new int[]{2}), // 3 p2
-            new ComponentInfo(false, new int[]{3}), // 4 "refresher"
-            ComponentInfo.EMPTY_NORMAL, // 5 eventHandler
-            new ComponentInfo(false, new int[]{5}), // 6 "refresh"
-            new ComponentInfo(false, new int[]{1, 4, 6}), // 7 layout
-            new ComponentInfo(false, new int[]{7}), // 8 view
+            ComponentInfo.EMPTY_NORMAL, // 4 text3
+            new ComponentInfo(true, new int[]{4}), // 5 p3
+            ComponentInfo.EMPTY_NORMAL, // 6 text4
+            new ComponentInfo(true, new int[]{6}), // 7 p4
+            new ComponentInfo(false, new int[]{3, 5, 7}), // 8 "refresher"
+            ComponentInfo.EMPTY_NORMAL, // 9 eventHandler
+            new ComponentInfo(false, new int[]{9}), // 10 "refresh"
+            new ComponentInfo(false, new int[]{1, 8, 10}), // 11 layout
+            new ComponentInfo(false, new int[]{11}), // 12 view
         };
         
         public NondominoPage() {
-            super(8, s_infos );
+            super(12, s_infos );
         }
         
         public int getComponentForId(String id) throws NoSuchComponentException { 
             if( "refresher".equals(id) )
-                return 4;
+                return 8;
             if( "refresh".equals(id) )
-                return 6;
+                return 10;
             return -1;
         }
         
@@ -66,23 +69,31 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
                 UIComponent parent, PageExpressionEvaluator evaluator)
                 throws NoSuchComponentException { 
             switch (id) {
-            case 8:
+            case 12:
                 return createView(context, parent, evaluator);
-            case 7:
+            case 11:
                 return createLayout(context, parent, evaluator);
             case 1:
                 return createP(context, parent, evaluator);
             case 0:
                 return createText(context, parent, evaluator);
-            case 4:
+            case 8:
                 return createRefresher(context, parent, evaluator);
             case 3:
                 return createP2(context, parent, evaluator);
             case 2:
                 return createText2(context, parent, evaluator);
-            case 6:
-                return createRefresh(context, parent, evaluator);
             case 5:
+                return createP3(context, parent, evaluator);
+            case 4:
+                return createText3(context, parent, evaluator);
+            case 7:
+                return createP4(context, parent, evaluator);
+            case 6:
+                return createText4(context, parent, evaluator);
+            case 10:
+                return createRefresh(context, parent, evaluator);
+            case 9:
                 return createEventHandler(context, parent, evaluator);
             }
             throw new NoSuchComponentException(id);
@@ -131,9 +142,9 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
 
         private UIComponent createText(FacesContext context, 
                 UIComponent parent, PageExpressionEvaluator evaluator) {
-            UIPassThroughText textComp = new UIPassThroughText();
-            textComp.setText("Page loaded!");
-            return textComp;
+            XspOutputText result = new XspOutputText();
+            result.setValue("Page loaded!");
+            return result;
         }
 
         private UIComponent createRefresher(FacesContext context, 
@@ -153,9 +164,42 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
         private UIComponent createText2(FacesContext context, 
                 UIComponent parent, PageExpressionEvaluator evaluator) {
             XspOutputText result = new XspOutputText();
-            result.setContentType("HTML");
+            String valueBnd = "Loaded at #{javascript: java.lang.System.currentTimeMillis().toString()}";
             String sourceId = "refresher/p[1]/xp:text[1]/@value";
-            String valueExpr = "Random string: #{randomString.randomString}<br />Theme: #{javascript:context.getProperty('xsp.theme')}";
+            Object value = evaluator.getBindingValue(result, valueBnd, sourceId, Object.class);
+            evaluator.setValue(result, "value", value, false, false);
+            return result;
+        }
+
+        private UIComponent createP3(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("p");
+            return component;
+        }
+
+        private UIComponent createText3(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspOutputText result = new XspOutputText();
+            String sourceId = "refresher/p[2]/xp:text[1]/@value";
+            String valueExpr = "Refresher refreshed at #{javascript: java.lang.System.currentTimeMillis().toString()}";
+            ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId,Object.class);
+            result.setValueBinding("value", value);
+            return result;
+        }
+
+        private UIComponent createP4(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("p");
+            return component;
+        }
+
+        private UIComponent createText4(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspOutputText result = new XspOutputText();
+            String sourceId = "refresher/p[3]/xp:text[1]/@value";
+            String valueExpr = "Random string: #{randomString.randomString}";
             ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId,Object.class);
             result.setValueBinding("value", value);
             return result;
