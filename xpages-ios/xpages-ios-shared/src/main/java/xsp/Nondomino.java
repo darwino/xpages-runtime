3a2186eb7a9b72a31897d440c5faf51f490ba369
Jesse Gallagher <jesse@secondfoundation.org>
2019-04-06 14:10:23 -0400

    Move theme to WEB-INF/themes

--- xpages-ios/xpages-ios-shared/src/main/java/xsp/Nondomino.java
+++ xpages-ios/xpages-ios-shared/src/main/java/xsp/Nondomino.java
@@ -23,6 +23,8 @@ import java.lang.Object;
 import javax.faces.el.ValueBinding;
 import com.ibm.xsp.component.xp.XspCommandButton;
 import com.ibm.xsp.component.xp.XspEventHandler;
+import com.ibm.xsp.component.UIPassThroughText;
+import java.lang.Boolean;
 
 @SuppressWarnings("all")
 public class Nondomino extends AbstractCompiledPageDispatcher{
@@ -49,15 +51,37 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
             new ComponentInfo(false, new int[]{3, 5, 7}), // 8 "refresher"
             ComponentInfo.EMPTY_NORMAL, // 9 eventHandler
             new ComponentInfo(false, new int[]{9}), // 10 "refresh"
-            new ComponentInfo(false, new int[]{1, 8, 10}), // 11 layout
-            new ComponentInfo(false, new int[]{11}), // 12 view
+            ComponentInfo.EMPTY_MARKUP, // 11 text5
+            new ComponentInfo(true, new int[]{11}), // 12 legend
+            ComponentInfo.EMPTY_MARKUP, // 13 text6
+            new ComponentInfo(true, new int[]{13}), // 14 dt
+            ComponentInfo.EMPTY_NORMAL, // 15 text7
+            new ComponentInfo(true, new int[]{15}), // 16 dd
+            ComponentInfo.EMPTY_MARKUP, // 17 text8
+            new ComponentInfo(true, new int[]{17}), // 18 dt2
+            ComponentInfo.EMPTY_NORMAL, // 19 text9
+            new ComponentInfo(true, new int[]{19}), // 20 dd2
+            ComponentInfo.EMPTY_MARKUP, // 21 text10
+            new ComponentInfo(true, new int[]{21}), // 22 dt3
+            ComponentInfo.EMPTY_NORMAL, // 23 text11
+            new ComponentInfo(true, new int[]{23}), // 24 dd3
+            ComponentInfo.EMPTY_MARKUP, // 25 text12
+            new ComponentInfo(true, new int[]{25}), // 26 dt4
+            ComponentInfo.EMPTY_NORMAL, // 27 text13
+            new ComponentInfo(true, new int[]{27}), // 28 dd4
+            new ComponentInfo(true, new int[]{14, 16, 18, 20, 22, 24, 26, 28}), // 29 dl
+            new ComponentInfo(true, new int[]{12, 29}), // 30 fieldset
+            new ComponentInfo(false, new int[]{1, 8, 10, 30}), // 31 "layout"
+            new ComponentInfo(false, new int[]{31}), // 32 view
         };
         
         public NondominoPage() {
-            super(12, s_infos );
+            super(32, s_infos );
         }
         
         public int getComponentForId(String id) throws NoSuchComponentException { 
+            if( "layout".equals(id) )
+                return 31;
             if( "refresher".equals(id) )
                 return 8;
             if( "refresh".equals(id) )
@@ -69,9 +93,9 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
                 UIComponent parent, PageExpressionEvaluator evaluator)
                 throws NoSuchComponentException { 
             switch (id) {
-            case 12:
+            case 32:
                 return createView(context, parent, evaluator);
-            case 11:
+            case 31:
                 return createLayout(context, parent, evaluator);
             case 1:
                 return createP(context, parent, evaluator);
@@ -95,6 +119,46 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
                 return createRefresh(context, parent, evaluator);
             case 9:
                 return createEventHandler(context, parent, evaluator);
+            case 30:
+                return createFieldset(context, parent, evaluator);
+            case 12:
+                return createLegend(context, parent, evaluator);
+            case 11:
+                return createText5(context, parent, evaluator);
+            case 29:
+                return createDl(context, parent, evaluator);
+            case 14:
+                return createDt(context, parent, evaluator);
+            case 13:
+                return createText6(context, parent, evaluator);
+            case 16:
+                return createDd(context, parent, evaluator);
+            case 15:
+                return createText7(context, parent, evaluator);
+            case 18:
+                return createDt2(context, parent, evaluator);
+            case 17:
+                return createText8(context, parent, evaluator);
+            case 20:
+                return createDd2(context, parent, evaluator);
+            case 19:
+                return createText9(context, parent, evaluator);
+            case 22:
+                return createDt3(context, parent, evaluator);
+            case 21:
+                return createText10(context, parent, evaluator);
+            case 24:
+                return createDd3(context, parent, evaluator);
+            case 23:
+                return createText11(context, parent, evaluator);
+            case 26:
+                return createDt4(context, parent, evaluator);
+            case 25:
+                return createText12(context, parent, evaluator);
+            case 28:
+                return createDd4(context, parent, evaluator);
+            case 27:
+                return createText13(context, parent, evaluator);
             }
             throw new NoSuchComponentException(id);
         }
@@ -130,6 +194,7 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
             UIIncludeComposite result = new UIIncludeComposite();
             result.setPageDriver(getPageParam().getPageDriver());
             result.setPageName(resultPage);
+            setId(result, "layout");
             return result;
         }
 
@@ -220,6 +285,166 @@ public class Nondomino extends AbstractCompiledPageDispatcher{
             result.setSubmit(true);
             result.setEvent("onclick");
             result.setRefreshMode("partial");
+            return result;
+        }
+
+        private UIComponent createFieldset(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("fieldset");
+            return component;
+        }
+
+        private UIComponent createLegend(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("legend");
+            return component;
+        }
+
+        private UIComponent createText5(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughText textComp = new UIPassThroughText();
+            textComp.setText("Diag");
+            return textComp;
+        }
+
+        private UIComponent createDl(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dl");
+            return component;
+        }
+
+        private UIComponent createDt(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dt");
+            return component;
+        }
+
+        private UIComponent createText6(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughText textComp = new UIPassThroughText();
+            textComp.setText("View root renderer");
+            return textComp;
+        }
+
+        private UIComponent createDd(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dd");
+            return component;
+        }
+
+        private UIComponent createText7(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspOutputText result = new XspOutputText();
+            String sourceId = "layout/fieldset[1]/dl[1]/dd[1]/xp:text[1]/@value";
+            String valueExpr = "#{javascript: view.getRenderer(facesContext).getWrapped()}";
+            ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId,Object.class);
+            result.setValueBinding("value", value);
+            return result;
+        }
+
+        private UIComponent createDt2(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dt");
+            return component;
+        }
+
+        private UIComponent createText8(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughText textComp = new UIPassThroughText();
+            textComp.setText("xc:layout renderer");
+            return textComp;
+        }
+
+        private UIComponent createDd2(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dd");
+            return component;
+        }
+
+        private UIComponent createText9(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspOutputText result = new XspOutputText();
+            String sourceId = "layout/fieldset[1]/dl[1]/dd[2]/xp:text[1]/@value";
+            String valueExpr = "#{javascript: getComponent(\'layout\').getRenderer(facesContext).getWrapped()}";
+            ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId,Object.class);
+            result.setValueBinding("value", value);
+            return result;
+        }
+
+        private UIComponent createDt3(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dt");
+            return component;
+        }
+
+        private UIComponent createText10(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughText textComp = new UIPassThroughText();
+            textComp.setText("styleKit");
+            return textComp;
+        }
+
+        private UIComponent createDd3(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dd");
+            return component;
+        }
+
+        private UIComponent createText11(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            XspOutputText result = new XspOutputText();
+            String sourceId = "layout/fieldset[1]/dl[1]/dd[3]/xp:text[1]/@value";
+            String valueExpr = "#{facesContext.styleKit}";
+            ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId,Object.class);
+            result.setValueBinding("value", value);
+            return result;
+        }
+
+        private UIComponent createDt4(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dt");
+            return component;
+        }
+
+        private UIComponent createText12(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughText textComp = new UIPassThroughText();
+            textComp.setText("styleKit name");
+            return textComp;
+        }
+
+        private UIComponent createDd4(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            UIPassThroughTag component = new UIPassThroughTag();
+            component.setTag("dd");
+            return component;
+        }
+
+        private UIComponent createText13(FacesContext context, 
+                UIComponent parent, PageExpressionEvaluator evaluator) {
+            // loaded=??
+            String loadedBnd = "#{not empty facesContext.styleKit}";
+            String sourceId = "layout/fieldset[1]/dl[1]/dd[4]/xp:text[1]/@loaded";
+            String loaded = evaluator.evaluateAsString(null, loadedBnd, sourceId, boolean.class);
+            if ( null != loaded && loaded.length() > 0 &&
+                    ! Boolean.parseBoolean(loaded) ) {
+                return null;
+            } // else loaded was true
+            XspOutputText result = new XspOutputText();
+            String sourceId2 = "layout/fieldset[1]/dl[1]/dd[4]/xp:text[1]/@value";
+            String valueExpr = "#{facesContext.styleKit.name}";
+            ValueBinding value = evaluator.createValueBinding(result, valueExpr, sourceId2,Object.class);
+            result.setValueBinding("value", value);
             return result;
         }
 
