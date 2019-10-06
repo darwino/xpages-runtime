e7f805e2f455b6aa63c255a555d018de6f463861
Jesse Gallagher <jesse@secondfoundation.org>
2019-10-06 06:47:53 -0400

    Add dynamic .xsp page loader

--- xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpages/runtime/JakartaFacesApplication.java
+++ xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpages/runtime/JakartaFacesApplication.java
@@ -16,13 +16,28 @@
 package org.openntf.xpages.runtime;
 
 import com.ibm.commons.util.StringUtil;
+import com.ibm.commons.xml.DOMUtil;
+import com.ibm.commons.xml.XMLException;
 import com.ibm.xsp.application.ApplicationExImpl;
 import com.ibm.xsp.application.DesignerApplicationEx;
 import com.ibm.xsp.application.ViewHandlerEx;
 import com.ibm.xsp.application.ViewHandlerExImpl;
 import com.ibm.xsp.factory.FactoryLookup;
+import com.ibm.xsp.library.ClasspathResourceBundleSource;
 import com.ibm.xsp.library.FacesClassLoader;
 import com.ibm.xsp.page.FacesPageDriver;
+import com.ibm.xsp.registry.CompositeComponentDefinitionImpl;
+import com.ibm.xsp.registry.FacesLibraryImpl;
+import com.ibm.xsp.registry.FacesProject;
+import com.ibm.xsp.registry.FacesRegistry;
+import com.ibm.xsp.registry.FacesSharableRegistry;
+import com.ibm.xsp.registry.LibraryFragmentImpl;
+import com.ibm.xsp.registry.UpdatableLibrary;
+import com.ibm.xsp.registry.config.FacesClassLoaderFactory;
+import com.ibm.xsp.registry.config.IconUrlSource;
+import com.ibm.xsp.registry.config.ResourceBundleSource;
+import com.ibm.xsp.registry.parse.ConfigParser;
+import com.ibm.xsp.registry.parse.ConfigParserFactory;
 
 import javax.faces.application.Application;
 import javax.faces.application.ViewHandler;
@@ -30,8 +45,10 @@ import javax.faces.application.ViewHandler;
 import org.openntf.xpages.runtime.xsp.JakartaFacesClassLoader;
 import org.openntf.xpages.runtime.xsp.JakartaFacesPageDriver;
 import org.openntf.xpages.runtime.xsp.JakartaFactoryLookup;
+import org.w3c.dom.Document;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -40,6 +57,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -53,83 +71,57 @@ public class JakartaFacesApplication extends DesignerApplicationEx {
 	protected JakartaFacesApplication(Application application) {
 		super(application);
 
-		try {
-			Field factoryLookupField = ApplicationExImpl.class.getDeclaredField("_factoryLookup");
-			factoryLookupField.setAccessible(true);
-			factoryLookupField.set(this, getFactoryLookup());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		this.registerCustomControls();
+//		try {
+//			Field factoryLookupField = ApplicationExImpl.class.getDeclaredField("_factoryLookup");
+//			factoryLookupField.setAccessible(true);
+//			factoryLookupField.set(this, getFactoryLookup());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 	
-	private void registerCustomControls() {
-		URL controls = Thread.currentThread().getContextClassLoader().getResource("/WEB-INF/controls");
-		if(controls != null) {
-			if(log.isLoggable(Level.WARNING)) {
-				log.warning("searching for controls in " + controls);
-			}
-			
-			switch(StringUtil.toString(controls.getProtocol())) {
-			case "file":
-				try {
-					Path path = Paths.get(controls.toURI());
-					Files.find(path, 1, (file, attrs) ->
-						file.getFileName().toString().endsWith(".xsp-config")
-					).forEach(config -> {
-						System.out.println("registering " + config);
-					});
-				} catch (IOException | URISyntaxException e) {
-					throw new RuntimeException(e);
-				}
-				break;
-			case "jar":
-				// TODO figure out, and maybe account for "wsjar"
-				break;
-			}
-		}
-	}
+	
+	
 
-	@Override
-	public synchronized ViewHandler getViewHandler() {
-		if (this.viewHandler == null) {
-			ViewHandler sup = super.getViewHandler();
-			if (!(sup instanceof ViewHandlerEx)) {
-				FacesClassLoader cl = new JakartaFacesClassLoader(getDesignerApplication());
-				FacesPageDriver driver = new JakartaFacesPageDriver(cl);
-				ViewHandlerExImpl result = new ViewHandlerExImpl(sup);
-				result.setPageDriver(driver);
-				this.viewHandler = result;
-			} else {
-				this.viewHandler = sup;
-			}
-		}
-		return this.viewHandler;
-	}
+//	@Override
+//	public synchronized ViewHandler getViewHandler() {
+//		if (this.viewHandler == null) {
+//			ViewHandler sup = super.getViewHandler();
+//			if (!(sup instanceof ViewHandlerEx)) {
+//				FacesClassLoader cl = new JakartaFacesClassLoader(getDesignerApplication());
+//				FacesPageDriver driver = new JakartaFacesPageDriver(cl);
+//				ViewHandlerExImpl result = new ViewHandlerExImpl(sup);
+//				result.setPageDriver(driver);
+//				this.viewHandler = result;
+//			} else {
+//				this.viewHandler = sup;
+//			}
+//		}
+//		return this.viewHandler;
+//	}
 
-	@Override
-	public FactoryLookup getFactoryLookup() {
-		if (this.factoryLookup == null) {
-			this.factoryLookup = new JakartaFactoryLookup(super.getFactoryLookup());
-		}
-		return this.factoryLookup;
-	}
+//	@Override
+//	public FactoryLookup getFactoryLookup() {
+//		if (this.factoryLookup == null) {
+//			this.factoryLookup = new JakartaFactoryLookup(super.getFactoryLookup());
+//		}
+//		return this.factoryLookup;
+//	}
 
-	@Override
-	public String getApplicationProperty(String s, String s1) {
-		String prop = super.getApplicationProperty(s, s1);
-		return prop;
-	}
+//	@Override
+//	public String getApplicationProperty(String s, String s1) {
+//		String prop = super.getApplicationProperty(s, s1);
+//		return prop;
+//	}
+//
+//	@Override
+//	protected ResourceBundle loadResourceBundle(String s, Locale locale) throws IOException {
+//		return super.loadResourceBundle(s, locale);
+//	}
 
-	@Override
-	protected ResourceBundle loadResourceBundle(String s, Locale locale) throws IOException {
-		return super.loadResourceBundle(s, locale);
-	}
-
-	@Override
-	public List<?> findServices(String s) {
-		List<?> svc = super.findServices(s);
-		return svc;
-	}
+//	@Override
+//	public List<?> findServices(String s) {
+//		List<?> svc = super.findServices(s);
+//		return svc;
+//	}
 }
---
+++ xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpages/runtime/servlet/JakartaServlet.java
@@ -0,0 +1,81 @@
+package org.openntf.xpages.runtime.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openntf.xpages.runtime.platform.JakartaPlatform;
+import org.openntf.xpages.runtime.wrapper.JakartaServletConfigWrapper;
+import org.openntf.xpages.runtime.wrapper.JakartaServletRequestWrapper;
+
+import com.ibm.commons.util.StringUtil;
+import com.ibm.xsp.webapp.FacesResourceServlet;
+import com.ibm.xsp.webapp.resources.JavaResourceProvider;
+import com.ibm.xsp.webapp.resources.Resource;
+
+@WebServlet(value="/")
+public class JakartaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static ServletConfig servletConfig;
+
+	private JakartaFacesServlet delegate;
+	private FacesResourceServlet resources = new FacesResourceServlet() {
+		private static final long serialVersionUID = 1L;
+		
+	}; 
+
+	public JakartaServlet() {
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		servletConfig = config;
+		
+		this.delegate = new JakartaFacesServlet();
+		
+		ServletConfig conf = new JakartaServletConfigWrapper(config);
+		JakartaPlatform.initContext(conf.getServletContext());
+		delegate.init(conf);
+		
+		resources.addResourceProvider(new JavaResourceProvider("") {
+			@Override
+			public Resource getResource(HttpServletRequest paramHttpServletRequest, String paramString) {
+				return super.getResource(paramHttpServletRequest, paramString);
+			}
+			@Override
+			protected String getResourcePath(HttpServletRequest req, String path) {
+				if(Thread.currentThread().getContextClassLoader().getResourceAsStream(path) != null) {
+					return path;
+				} else {
+					return null;
+				}
+			}
+		});
+		resources.init(conf);
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = StringUtil.toString(req.getServletPath());
+		int xspIndex = path.indexOf(".xsp");
+		if(xspIndex > -1) {
+			String pathInfo = path.substring(xspIndex+4);
+			HttpServletRequest wrap = new JakartaServletRequestWrapper(req, path.substring(0, xspIndex+4), pathInfo.isEmpty() ? null : pathInfo);
+			delegate.service(wrap, resp);
+		} else {
+			HttpServletRequest wrap = new JakartaServletRequestWrapper(req, "/", path);
+			resources.service(wrap, resp);
+		}
+	}
+
+	@Override
+	public void destroy() {
+		delegate.destroy();
+	}
+}
