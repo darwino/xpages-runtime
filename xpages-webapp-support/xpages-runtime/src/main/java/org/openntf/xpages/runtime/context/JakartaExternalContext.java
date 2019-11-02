5aae6cd96bf63e7efc574c917329a0721221e025
Jesse Gallagher <jesse@secondfoundation.org>
2019-11-02 11:35:30 -0400

    Add flag to disable HTTP/2 push in JakartaExternalContext

--- xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpages/runtime/context/JakartaExternalContext.java
+++ xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpages/runtime/context/JakartaExternalContext.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.openntf.xpages.runtime.platform.JakartaPlatform;
 
 import com.ibm.commons.util.StringUtil;
+import com.ibm.xsp.application.ApplicationEx;
 import com.ibm.xsp.context.ExternalContextEx;
 import com.sun.faces.RIConstants;
 
@@ -53,78 +54,86 @@ public class JakartaExternalContext extends ExternalContextEx {
 	
 	private static final String PUSH_SUPPORTED_ATTRIBUTE_NAME = RIConstants.FACES_PREFIX + "ExternalContextImpl.PUSH_SUPPORTED";
 	public static final String PUSH_RESOURCE_URLS_KEY_NAME = RIConstants.FACES_PREFIX + "resourceUrls";
+	public static final String XSP_ENABLE_PUSH = "xsp.http2.push";
 	
-    private void pushIfPossibleAndNecessary(String result) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        ExternalContext extContext = context.getExternalContext();
-        HttpServletRequest req = (HttpServletRequest)extContext.getRequest();
-        Object val;
-        
-        // 1. check the request cache
-        if (null != (val = req.getAttribute(PUSH_SUPPORTED_ATTRIBUTE_NAME))) {
-            if (!(Boolean) val) {
-                return;
-            }
-        }
-        
-        // 2. Not in the request cache, see if PushBuilder is available in the container
-        // Not available in XPages's JSF fork
+	private void pushIfPossibleAndNecessary(String result) {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		if ("false".equals(ApplicationEx.getInstance(context).getProperty(XSP_ENABLE_PUSH, "true"))) {
+			return;
+		}
+
+		ExternalContext extContext = context.getExternalContext();
+		HttpServletRequest req = (HttpServletRequest) extContext.getRequest();
+		Object val;
+
+		// 1. check the request cache
+		if (null != (val = req.getAttribute(PUSH_SUPPORTED_ATTRIBUTE_NAME))) {
+			if (!(Boolean) val) {
+				return;
+			}
+		}
+
+		// 2. Not in the request cache, see if PushBuilder is available in the container
+		// Not available in XPages's JSF fork
 //        ApplicationAssociate associate = ApplicationAssociate.getInstance(extContext);
 //        if (!associate.isPushBuilderSupported()) {
 //            // At least we won't have to hit the ApplicationAssociate every time on this request.
 //            attrs.putIfAbsent(PUSH_SUPPORTED_ATTRIBUTE_NAME, FALSE);
 //            return;
 //        }
-        
-        // 3. Don't bother trying to push if we've already pushed this URL for this request
-        @SuppressWarnings("unchecked")
-		Set<String> resourceUrls = (Set<String>)req.getAttribute(PUSH_RESOURCE_URLS_KEY_NAME);
-        if(resourceUrls == null) {
-        	resourceUrls = new HashSet<>();
-        	req.setAttribute(PUSH_RESOURCE_URLS_KEY_NAME, resourceUrls);
-        }
-        if (resourceUrls.contains(result)) {
-            return;
-        }
-        resourceUrls.add(result);
-        
-        // 4. At this point we know 
-        // a) the container has PushBuilder
-        // b) we haven't pushed this URL for this request before
-        Object pbObj = getPushBuilder(context, extContext);
-        if (pbObj != null) {
-            // and now we also know c) there was no If-Modified-Since header
-            ((javax.servlet.http.PushBuilder)pbObj).path(result).push();
-        }
-        
-    }
-	
-    private Object getPushBuilder(FacesContext context, ExternalContext extContext) {
-        javax.servlet.http.PushBuilder result = null;
-        
-        Object requestObj = extContext.getRequest();
-        if (requestObj instanceof HttpServletRequest) {
-            HttpServletRequest hreq = (HttpServletRequest) requestObj;
-            Object val;
-            boolean isPushSupported = false;
-            
-            // Try to pull value from the request cache
-            if ((val = hreq.getAttribute(PUSH_SUPPORTED_ATTRIBUTE_NAME)) != null) {
-                isPushSupported = (Boolean) val;
-            } else {
-                // If the request has an If-Modified-Since header, do not push, since it's 
-                // possible the resources are already in the cache.
-                isPushSupported = StringUtil.isEmpty(StringUtil.toString(extContext.getRequestHeaderMap().get("If-Modified-Since")));
-            }
-            
-            if (isPushSupported) {
-                isPushSupported = (result = hreq.newPushBuilder()) != null;
-            }
-            if(!Collections.list(hreq.getAttributeNames()).contains(PUSH_SUPPORTED_ATTRIBUTE_NAME)) {
-            	hreq.setAttribute(PUSH_SUPPORTED_ATTRIBUTE_NAME, isPushSupported);
-            }
-        }
-        
-        return result;
-    }
+
+		// 3. Don't bother trying to push if we've already pushed this URL for this
+		// request
+		@SuppressWarnings("unchecked")
+		Set<String> resourceUrls = (Set<String>) req.getAttribute(PUSH_RESOURCE_URLS_KEY_NAME);
+		if (resourceUrls == null) {
+			resourceUrls = new HashSet<>();
+			req.setAttribute(PUSH_RESOURCE_URLS_KEY_NAME, resourceUrls);
+		}
+		if (resourceUrls.contains(result)) {
+			return;
+		}
+		resourceUrls.add(result);
+
+		// 4. At this point we know
+		// a) the container has PushBuilder
+		// b) we haven't pushed this URL for this request before
+		Object pbObj = getPushBuilder(context, extContext);
+		if (pbObj != null) {
+			// and now we also know c) there was no If-Modified-Since header
+			((javax.servlet.http.PushBuilder) pbObj).path(result).push();
+		}
+
+	}
+
+	private Object getPushBuilder(FacesContext context, ExternalContext extContext) {
+		javax.servlet.http.PushBuilder result = null;
+
+		Object requestObj = extContext.getRequest();
+		if (requestObj instanceof HttpServletRequest) {
+			HttpServletRequest hreq = (HttpServletRequest) requestObj;
+			Object val;
+			boolean isPushSupported = false;
+
+			// Try to pull value from the request cache
+			if ((val = hreq.getAttribute(PUSH_SUPPORTED_ATTRIBUTE_NAME)) != null) {
+				isPushSupported = (Boolean) val;
+			} else {
+				// If the request has an If-Modified-Since header, do not push, since it's
+				// possible the resources are already in the cache.
+				isPushSupported = StringUtil
+						.isEmpty(StringUtil.toString(extContext.getRequestHeaderMap().get("If-Modified-Since")));
+			}
+
+			if (isPushSupported) {
+				isPushSupported = (result = hreq.newPushBuilder()) != null;
+			}
+			if (!Collections.list(hreq.getAttributeNames()).contains(PUSH_SUPPORTED_ATTRIBUTE_NAME)) {
+				hreq.setAttribute(PUSH_SUPPORTED_ATTRIBUTE_NAME, isPushSupported);
+			}
+		}
+
+		return result;
+	}
 }
